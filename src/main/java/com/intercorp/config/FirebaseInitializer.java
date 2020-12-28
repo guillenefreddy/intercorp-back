ec2f4a04711e5523df23dfa63e6e80230a143e21
Freddy Guillén Escobar <fguillee@everis.com>
2020-12-28 15:22:35 -0500

    compiler

--- src/main/java/com/intercorp/config/FirebaseInitializer.java
+++ src/main/java/com/intercorp/config/FirebaseInitializer.java
@@ -1,13 +1,12 @@
 package com.intercorp.config;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URISyntaxException;
-import java.net.URL;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
 import com.google.auth.oauth2.GoogleCredentials;
@@ -21,16 +20,10 @@ public class FirebaseInitializer {
 	@PostConstruct
 	public void initialize() throws IOException, URISyntaxException{
 		
-		System.out.println("PASO");
-		URL serviceAccount = getClass().getResource("/serviceAccountKey.json");
-		System.out.println("VALOR"+ serviceAccount);
-		File file = new File(serviceAccount.toURI());
-		FileInputStream input = new FileInputStream(file);
-		
-		System.out.println("input");
-
+		InputStream serviceAccount = new ClassPathResource("serviceAccountKey.json").getInputStream();
+				
 		FirebaseOptions options = new FirebaseOptions.Builder()
-		  .setCredentials(GoogleCredentials.fromStream(input))
+		  .setCredentials(GoogleCredentials.fromStream(serviceAccount))
 		  .setDatabaseUrl("https://retointercorp-8abe5-default-rtdb.firebaseio.com")
 		  .build();
 
