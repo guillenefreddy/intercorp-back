8494b60420fa591a79daf881af7b60b6b495c171
Freddy Guillén Escobar <fguillee@everis.com>
2020-12-28 14:07:17 -0500

    compiler

--- src/main/java/com/intercorp/config/FirebaseInitializer.java
+++ src/main/java/com/intercorp/config/FirebaseInitializer.java
@@ -21,10 +21,14 @@ public class FirebaseInitializer {
 	@PostConstruct
 	public void initialize() throws IOException, URISyntaxException{
 		
+		System.out.println("PASO");
 		URL serviceAccount = getClass().getResource("/serviceAccountKey.json");
+		System.out.println("VALOR"+ serviceAccount);
 		File file = new File(serviceAccount.toURI());
 		FileInputStream input = new FileInputStream(file);
 		
+		System.out.println("input");
+
 		FirebaseOptions options = new FirebaseOptions.Builder()
 		  .setCredentials(GoogleCredentials.fromStream(input))
 		  .setDatabaseUrl("https://retointercorp-8abe5-default-rtdb.firebaseio.com")
